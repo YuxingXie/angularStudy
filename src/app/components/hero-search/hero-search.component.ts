ef7af8c9cc5e836a2982c15c30791d415e34979c
Xieyuxing <185246042@qq.com>
2018-11-24 22:55:46 +0800

    aaaa

--- src/app/components/hero-search/hero-search.component.ts
+++ src/app/components/hero-search/hero-search.component.ts
@@ -1,25 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { Observable, Subject } from 'rxjs';
+import {Observable, Subject} from 'rxjs';
 
 import {
   debounceTime, distinctUntilChanged, switchMap
 } from 'rxjs/operators';
 
-import { Hero } from 'app/entities/hero';
-import { HeroService } from 'app/services/hero.service';
+import {Hero} from 'app/entities/hero';
+import {HeroService} from 'app/services/hero.service';
 
 @Component({
   selector: 'app-hero-search',
   templateUrl: './hero-search.component.html',
-  styleUrls: [ './hero-search.component.css' ],
+  styleUrls: ['./hero-search.component.css'],
   providers: [HeroService]
 })
 export class HeroSearchComponent implements OnInit {
   heroes$: Observable<Hero[]>;
   private searchTerms = new Subject<string>();
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService) {
+  }
 
   // Push a search term into the observable stream.
   search(term: string): void {
